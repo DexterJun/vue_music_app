f02c8fee1f5bf95a69c840d6c5f80df8f9f86198
DexterJun <1300722971@qq.com>
2022-01-28 00:29:57 +0800

    添加歌手页面

--- src/components/base/scroll/Scroll.vue
+++ src/components/base/scroll/Scroll.vue
@@ -11,16 +11,26 @@ import useScroll from './useScroll'
 
 export default {
   name: 'scroll',
-  props: { // 为组件添加默认的点击事件
+  props: {
+    // 为组件添加默认的点击事件
     click: {
       type: Boolean,
       default: true
+    },
+    /* probeType 为 0，在任何时候都不派发 scroll 事件
+    probeType 为 1，仅仅当手指按在滚动区域上，每隔 momentumLimitTime 毫秒派发一次 scroll 事件
+    probeType 为 2，仅仅当手指按在滚动区域上，一直派发 scroll 事件
+    probeType 为 3，任何时候都派发 scroll 事件，包括调用 scrollTo 或者触发 momentum 滚动动画 */
+    probeType: {
+      type: Number,
+      default: 0
     }
   },
-  setup(props) {
+  emits: ['scroll'], // 向外派发一个名为scroll的自定义事件
+  setup(props, { emit }) {
     const rootRef = ref(null)
-    useScroll(rootRef, props)
-    return { rootRef }
+    const scroll = useScroll(rootRef, props, emit)
+    return { rootRef, scroll }
   }
 }
 </script>
--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -1,15 +1,34 @@
 <template>
-  <div class="singer">
-    歌手页面
+  <div class="singer" v-loading="!singers.length">
+    <IndexList :data="singers"></IndexList>
   </div>
 </template>
 
 <script>
+import { getSingerList } from '../service/singer'
+import IndexList from '../components/base/indexList/IndexList.vue'
+
 export default {
-  name: 'singer'
+  name: 'singer',
+  components: { IndexList },
+  data() {
+    return {
+      singers: []
+    }
+  },
+  async created() {
+    const result = await getSingerList()
+    console.log(result)
+    this.singers = result.singers
+  }
 }
 </script>
 
 <style lang='scss' scoped>
-
+  .singer {
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+  }
 </style>
